0ddd9f8ff6bd32a52aeb6e0bb6c98c2b6ebb55b7
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-12-12 11:17:23 +0100

    pretty confusing tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { CommentComponent } from './comment/comment.component';
+import { SuggestionsComponent } from './suggestions/suggestions.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentComponent,
+    SuggestionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
